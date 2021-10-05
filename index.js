aa94a6c1f319b74bf0be703e7564996b8a716afa
Alexandria DAntonio <adantonio@wayne.edu>
2021-10-05 15:17:56 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "alex", 
+    streetAddress : "123 Gardenia Avenue"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key]; //using delete newEmployee.key does not work
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+
+    return employee;
+}
