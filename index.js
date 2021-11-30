7f6dc94a51ccb149bf4d1233eb1c0f87d8ae6230
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-11-30 01:16:21 -0500

    commit tree 36dec57c3b4b640fe2be0ce491858b422ceebbdb

--- index.js
+++ index.js
@@ -1,32 +1,83 @@
 // Write your solution in this file!
 
 const employee = {
-    name : "alex", 
-    streetAddress : "123 Gardenia Avenue"
+    name : "Alex",
+    streetAdress : "1234 Cutty Drive",
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee = {...employee};
-    newEmployee[key] = value;
-
-    return newEmployee;
+    return {
+        ...employee,
+        [key] : value, //make sure it's [key] and not just key
+    };
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value;
-
     return employee;
 }
 
 function deleteFromEmployeeByKey(employee, key){
-    const newEmployee = {...employee};
-    delete newEmployee[key]; //using delete newEmployee.key does not work
-
+    const newEmployee = {
+        ...employee,
+    }
+    delete newEmployee[key];
     return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee,key){
+function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key];
-
     return employee;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name : "alex", 
+//     streetAddress : "123 Gardenia Avenue"
+// };
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newEmployee = {...employee};
+//     newEmployee[key] = value;
+
+//     return newEmployee;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key] = value;
+
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//     const newEmployee = {...employee};
+//     delete newEmployee[key]; //using delete newEmployee.key does not work
+
+//     return newEmployee;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee,key){
+//     delete employee[key];
+
+//     return employee;
+// }
